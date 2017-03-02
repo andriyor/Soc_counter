2f4e607e285f2190225c2274826b682c6e155ac1
Andriy Orehov <andriyorehov@gmail.com>
2017-03-02 22:51:50 +0200

    asynchrony not work from view function

--- Soc_counter.py
+++ Soc_counter.py
@@ -1,45 +1,38 @@
 import time
 from flask import Flask, render_template
-import requests
+import asyncio
 from bs4 import BeautifulSoup
+from aiohttp import ClientSession
 
 app = Flask(__name__)
 
 handle = 'rozetked'
 
 
-def get_youtube_subscriber():
-    url = "https://www.youtube.com/user/{}/about?hl=en".format(handle)
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
+def get_youtube_subscriber(page):
+    soup = BeautifulSoup(page, 'html.parser')
     about_stat = soup.find_all("span", {"class": "about-stat"})
     ys, bl, jl, = map(lambda x: x.text, about_stat)
     ys = ys.split()[0]
     return ys
 
 
-def get_twitter_followers():
-
-    url = "https://twitter.com/{}".format(handle)
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+def get_twitter_followers(page):
+    soup = BeautifulSoup(page, 'html.parser')
     followers = soup.find('li', {'class': 'ProfileNav-item ProfileNav-item--followers'}) \
         .find('span', {'class': 'ProfileNav-value'}).text
     return followers
 
 
-def get_instagram_followers():
-    url = "https://www.instagram.com/{}".format(handle)
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
+def get_instagram_followers(page):
+    soup = BeautifulSoup(page, 'html.parser')
     followers = soup.find("meta", property="og:description")["content"]
     followers = followers.split()[0]
     return followers
 
 
-def get_facebook_followers():
-    url = "https://www.facebook.com/{}".format(handle)
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
+def get_facebook_followers(page):
+    soup = BeautifulSoup(page, 'html.parser')
     desc = soup.find(attrs={"name": "description"})["content"]
     descl = desc.split()
 
@@ -57,27 +50,39 @@ def get_facebook_followers():
     return followers
 
 
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.read()
+
+
+async def run():
+    tasks = []
+    async with ClientSession() as session:
+        link_list = ["https://www.youtube.com/user/{}/about?hl=en".format(handle),
+                     "https://twitter.com/{}".format(handle),
+                     "https://www.instagram.com/{}".format(handle),
+                     "https://www.facebook.com/{}".format(handle)]
+
+        for link in link_list:
+            task = asyncio.ensure_future(fetch(link, session))
+            tasks.append(task)
+
+        return await asyncio.gather(*tasks)
+
+
 @app.route('/')
 def hello_world():
     start_time = time.time()
-    ys = get_youtube_subscriber()
-    print("--- %s get_youtube_subscriber ---" % (time.time() - start_time))
-
-    start_time = time.time()
-    tf = get_twitter_followers()
-    print("--- %s get_twitter_followers ---" % (time.time() - start_time))
-
-    start_time = time.time()
-    instagram_followers = get_instagram_followers()
-    print("--- %s get_instagram_followers ---" % (time.time() - start_time))
-
-    start_time = time.time()
-    ff = get_facebook_followers()
-    print("--- %s get_facebook_followers ---" % (time.time() - start_time))
-
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run())
+    y, t, i, f = loop.run_until_complete(future)
+    ys = get_youtube_subscriber(y)
+    tf = get_twitter_followers(t)
+    instagram_followers = get_instagram_followers(i)
+    ff = get_facebook_followers(f)
+    print("--- %s get ---" % (time.time() - start_time))
     return render_template("index.html", ys=ys, tf=tf, instagram_followers=instagram_followers, ff=ff)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
